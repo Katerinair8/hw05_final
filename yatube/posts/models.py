992d54fc4526a766ef173c0f4bb500ccfbcb096c
Екатерина Василевич <katerinair8@mail.ru>
2022-09-22 23:43:21 +0200

    спринт 6. коммит после первого ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -113,7 +113,7 @@ class Follow(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['user','author'],
+                fields=['user', 'author'],
                 name='unique_user_author'
             )
         ]
